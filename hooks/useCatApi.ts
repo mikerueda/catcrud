8e10b88aa957ee83d20dd89f1fcfa45629236a30
Mike Rueda <marcreativo@gmail.com>
2021-08-18 17:44:09 -0300

    Feature/use cat hook (#3)
    * base hook for cats api
    
    * delete actions and base button
    
    * add button

---
+++ hooks/useCatApi.ts
@@ -0,0 +1,35 @@
+import {useState, useEffect} from 'react'
+import {Cat} from 'helpers/types'
+import Api from 'helpers/axios'
+
+export default (initialCats:number) => {
+  const [cats, setCats] = useState<Cat[]>([])
+  const [nextPage, setNextPage] = useState() 
+  // const [error, setError] = useState()
+
+  const getCats = async (count?:number, nextItems?:number) => {
+    const {data:{items, next}} = await Api.get(`?count=${count || 5}${nextItems ? `?next=${nextItems}`: ''}`)
+    setCats(items)
+    setNextPage(next)
+  }
+
+  const deleteCat = async (id) => {
+    await Api.delete(`/${id}`)
+    getCats(initialCats)
+  }
+
+  const createCat = (cat) => {
+    
+  }
+
+  useEffect(()=>{
+    getCats(initialCats)
+  }, [])
+
+  return{
+    cats,
+    getCats,
+    deleteCat,
+    createCat
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,35 @@
-import {useEffect, useState} from 'react'
 import Head from 'next/head';
 import Image from 'next/image';
-import Api from 'helpers/axios'
 import CatItem from 'components/CatItem';
-import {Cat} from 'helpers/types'
+import useCatApi from 'hooks/useCatApi'
+import Button from 'components/Button'
 
 const Home = () => {
-  const [cats, setCats] = useState<Cat[]>([])
-  const [nextPage, setNextPage] = useState() 
-
-  useEffect(()=>{
-    const fetchCats = async () => {
-      const {data:{items, next}} = await Api.get('/')
-      setCats(items)
-      setNextPage(next)
-    }
-
-    fetchCats()
-  }, [])
+  const {cats, deleteCat} = useCatApi(10)
 
   return (
-    <div className="bg-purple-400 min-h-full">
+    <div className="bg-purple-400 min-h-screen ">
       <Head>
         <title>CatsCrud</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className='container mx-auto p-4'>
-        <header className='flex flex-row my-4 text-2xl leading-4 text-yellow-200 font-black items-center	'>
-          <Image src="/logo.png" alt="logo" width="30" height="30"/>
-          <h1>CatsCrud</h1>
+
+      <div className='container mx-auto p-4 md:max-w-md'>
+        <header className='flex flex-row justify-between my-4 text-2xl leading-4 text-yellow-200 font-black items-center'>
+          <div className='flex flex-row items-center'>
+            <Image src="/logo.png" alt="logo" width="30" height="30"/>
+            <h1>CatsCrud</h1>
+          </div>
+          <Button variant='green' onClick={()=>{}} >Add new Cat :3</Button>
         </header>
+
         <ul>
-          {cats.map( (cat, i) => <CatItem key={i} cat={cat}/>)}
+          {cats.map( (cat, i) => (
+            <CatItem 
+              key={i} 
+              cat={cat} 
+              deleteCat={deleteCat}
+            />))}
         </ul>
       </div>
     </div>
